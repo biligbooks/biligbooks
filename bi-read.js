1871c9f3c14fb0910593f2832b980bd1d8673c5d
biligbooks <biligbooks@gmail.com>
2012-06-10 23:58:49 -0700

    first commit

---
+++ bi-read.js
@@ -0,0 +1,11 @@
+var divs = 0;  
+
+function disDiv(div){
+    div.style.display="none"
+}
+
+function showDiv(did){  
+    var div=document.getElementById(did);
+    div.style.display = "block";
+};  
+
